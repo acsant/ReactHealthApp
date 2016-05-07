24694d4162a37236e851e42acb34bc6d9ea3e81f
Akash <acsant@uwaterloo.ca>
2016-05-07 16:34:36 -0400

    Loading images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ require('./_Header.scss');
 
 const Header = () => (
 	<div>
-		<img className= 'img__header' src="http://media.cmgdigital.com/shared/lt/lt_cache/thumbnail/400/img/photos/2014/06/04/fe/21/Washington-Department-of-Health-logo.jpg" />
+		<img className= 'img__header' src={require('../../images/header.svg')} />
 	</div>
+
 )
 
 export default Header
