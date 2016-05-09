0d9bdd7fac76a01aca400c3220949868fd29a1f8
Akash <acsant@uwaterloo.ca>
2016-05-08 23:34:28 -0400

    removed validate and login fail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,6 @@
 import { combineReducers } from 'redux';
 import login from './login';
-import loginFailed from './loginfail';
-import validate from './validate';
 
-const HealthApp = combineReducers(
-	login,
-	loginFailed,
-	validate
-)
+const HealthApp = login
 
 export default HealthApp
